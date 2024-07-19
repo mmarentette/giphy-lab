79b5bb9eb808f3af01a1ce96c99cb090b8a9b262
mmarentette <melrosemarentette@gmail.com>
2024-07-19 10:45:47 -0400

    Remove old comments

--- src/App.jsx
+++ src/App.jsx
@@ -7,14 +7,11 @@ export default function App() {
   const LIMIT = 25;
   const [gif, setGif] = useState({});
   const [searchGifTerm, setSearchGifTerm] = useState('dog');
-  const [isLoading, setIsLoading] = useState(true); // Question for Jim/Megan: Based on class example, this should be initialized to false, but I can't get it to work unless initialized to true... what am I missing here?
+  const [isLoading, setIsLoading] = useState(true);
 
   useEffect(() => {
 
     const endpoint = `https://api.giphy.com/v1/gifs/search?api_key=IP7R5AjVlJdI6wZPxyMJl0JSMX9jo7Sp&q=${searchGifTerm}&limit=${LIMIT}&lang=en`;
-
-    // Question for Jim/Megan: Is there a way to load a trending gif the first time the app loads, and then render a 'searched' gif every time searchGifTerm state is updated)? This is what I tried + initializing searchGifTerm state to empty string above:
-    // { const endpoint = searchGifTerm.length ? `https://api.giphy.com/v1/gifs/search?api_key=IP7R5AjVlJdI6wZPxyMJl0JSMX9jo7Sp&q=${searchGifTerm}&limit=${LIMIT}&lang=en`: `https://api.giphy.com/v1/gifs/trending?api_key=IP7R5AjVlJdI6wZPxyMJl0JSMX9jo7Sp&limit=${LIMIT}` };
 
     async function getGif() {
       try {
